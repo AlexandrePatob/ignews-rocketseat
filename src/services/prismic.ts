696ecef0b960fea835021457ca77fb3aba345e81
Alexandre Borges <ale13borges@gmail.com>
2022-10-07 14:58:47 -0300

    feat: ignews

---
+++ src/services/prismic.ts
@@ -0,0 +1,10 @@
+import * as Prismic from "@prismicio/client";
+import sm from "../../sm.json";
+
+export function getPrismicClient() {
+  const prismic = Prismic.createClient(sm.apiEndpoint, {
+    accessToken: process.env.PRISMIC_ACCES_TOKEN,
+  });
+
+  return prismic;
+}
